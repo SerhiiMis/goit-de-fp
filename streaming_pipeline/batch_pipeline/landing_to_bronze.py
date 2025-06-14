8a9279f902263c50d8604d0734bb4d6f1f0c39f5
Serhii Mishovych <serhiimishovych@gmail.com>
2025-06-14 07:19:31 +0200

    Bronze stage

---
+++ streaming_pipeline/batch_pipeline/landing_to_bronze.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+import os
+
+
+spark = SparkSession.builder \
+    .appName("LandingToBronze") \
+    .getOrCreate()
+
+tables = {
+    "athlete_bio": "batch_pipeline/data/athlete_bio.csv",
+    "athlete_event_results": "batch_pipeline/data/athlete_event_results.csv"
+}
+
+bronze_dir = "batch_pipeline/output/bronze"
+os.makedirs(bronze_dir, exist_ok=True)
+
+for name, path in tables.items():
+    print(f"📥 Reading {name} from {path}")
+    df = spark.read.option("header", "true").csv(path)
+
+    print(f"💾 Writing {name} to {bronze_dir}/{name}")
+    df.write.mode("overwrite").parquet(f"{bronze_dir}/{name}")
+
+    print(f"✅ Done: {name}")
+
+spark.stop()
