f226fdf3a258f27a42febe0bd8a9529bf10363aa
Serhii Mishovych <serhiimishovych@gmail.com>
2025-06-14 06:15:25 +0200

    Working Kafka producer-consumer pipeline

---
+++ streaming_pipeline/producer.py
@@ -0,0 +1,15 @@
+import csv
+import json
+from time import sleep
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+with open("data/athlete_event_results.csv", newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        producer.send("athlete_events", row)
+        sleep(0.1)  # throttle
